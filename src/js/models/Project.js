94384579b4de298c9e97e8110c0f22e546f796f9
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-05 13:18:42 +0200

    Create Project class and implement the constructor

---
+++ src/js/models/Project.js
@@ -0,0 +1,8 @@
+export default class Project {
+  constructor(title, description, dueDate) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.todos = [];
+  }
+}
