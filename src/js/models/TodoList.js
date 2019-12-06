78b86299e7fcccfdecfb143ae37200064b06f9d4
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-06 14:53:24 +0200

    Add id field to each todo which stores an uniq id

--- src/js/models/TodoList.js
+++ src/js/models/TodoList.js
@@ -1,10 +1,13 @@
+import uniqid from 'uniqid';
+
 export default class TodoList {
-  constructor(title, description, dueDate, priority = 'default', notes = [], checklist = []) {
+  constructor(title, description, dueDate, priority = 'default') {
+    this.id = uniqid();
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
-    this.notes = notes;
-    this.checklist = checklist;
   }
+
+
 }
