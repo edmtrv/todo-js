da6809447ec1ec070db9c8293ce1f51bc79e3187
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-06 20:07:06 +0200

    Implement a method to remove a todo from the list based on id

--- src/js/models/TodoList.js
+++ src/js/models/TodoList.js
@@ -7,6 +7,11 @@ export default class TodoList {
     this._todos.push(todo);
   }
 
+  removeTodo(id) {
+    const index = this._todos.findIndex(todo => todo.id === id);
+    this._todos.splice(index, 1);
+  }
+
   get todos() {
     return this._todos;
   }
