241a6c5086eb7d8b56c62cf91c0a25dbb8773b99
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-06 20:13:02 +0200

    Decided against using a separate TodoList object

--- src/js/models/Project.js
+++ src/js/models/Project.js
@@ -2,11 +2,23 @@ import TodoList from './TodoList';
 import uniqid from 'uniqid';
 
 export default class Project {
-  constructor(title, description, dueDate) {
+  constructor(title, description) {
     this.id = uniqid();
     this.title = title;
     this.description = description;
-    this.dueDate = dueDate;
-    this._todos = new TodoList();
+    this._todos = [];
+  }
+
+  addTodo(todo) {
+    this._todos.push(todo);
+  }
+
+  removeTodo(id) {
+    const index = this._todos.findIndex(todo => todo.id === id);
+    this._todos.splice(index, 1);
+  }
+
+  get todos() {
+    return this._todos;
   }
 }
