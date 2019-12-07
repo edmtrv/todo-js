2dc243e699f39ff0fe397e4888cb27dd1cdeb2d5
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-07 20:25:47 +0200

    Comment out test code

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,15 @@ import Todo from './models/Todo';
 const state = {};
 state.projects = [];
 
-const p1 = new Project('My first project', 'My project for testing purposes');
-const t1 = new Todo('My first todo', 'Doing something cool', '20191112');
-const t2 = new Todo('My second todo', 'A lot of work', '20191209');
-const t3 = new Todo('My third todo', 'Doing something even better', '20191125');
+// const p1 = new Project('My first project', 'My project for testing purposes');
+// const t1 = new Todo('My first todo', 'Doing something cool', '20191112');
+// const t2 = new Todo('My second todo', 'A lot of work', '20191209');
+// const t3 = new Todo('My third todo', 'Doing something even better', '20191125');
 
-p1.addTodo(t1);
-p1.addTodo(t2);
-p1.addTodo(t3);
+// p1.addTodo(t1);
+// p1.addTodo(t2);
+// p1.addTodo(t3);
 
-state.projects.push(p1);
+// state.projects.push(p1);
 
-console.dir(state);
+// console.dir(state);
