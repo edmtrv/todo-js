bdd43b476d5521fc4155db6e01d8bd9fce8fe61a
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-10 00:37:11 +0200

    Implement a function that creates the project modal  and puts it into the HTML

--- src/js/views/projectView.js
+++ src/js/views/projectView.js
@@ -10,6 +10,40 @@ export const renderProjectsList = (projects) => {
   elements.projectsList.innerHTML = markup;
 };
 
+export const createProjectModal = () => {
+  const markup = `
+    <div class="modal fade" id="project-modal" tabindex="-1" role="dialog" aria-labelledby="project-modal-label"
+    aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="project-modal-label">Project</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="title">Title</label>
+              <input type="text" name="title" id="title" aria-describedby="project-title">
+            </div>
+            <div class="form-group">
+              <label for="description">Description</label>
+              <input type="text" name="description" id="description" aria-describedby="project-description">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary btn-add-project">Add Project</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.body.appendChild(markup);
+};
+
 const renderProjectItem = (project) => {
   return `
     <button type="button" class="list-group-item list-group-item-action">
