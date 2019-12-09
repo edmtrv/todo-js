5699c3fe6a76a8a6747fafddf65c3c7856a73328
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-09 12:41:55 +0200

    Implement projects rendering function

--- src/js/views/projectView.js
+++ src/js/views/projectView.js
@@ -1,3 +1,9 @@
 export const renderProjects = (projects) => {
+  const markup = `
+    <ul class="list-group">
+      ${projects.forEach(project => renderProjects(project))};
+    </ul>
+  `;
 
+  document.getElementById('projects').innerHTML = markup;
 };
