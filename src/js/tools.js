5eda5dc61a11c7341f535d13b5437226f3e5b2a6
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-10 16:46:18 +0200

    Remove unnecessary button from elements object

--- src/js/tools.js
+++ src/js/tools.js
@@ -1,5 +1,4 @@
 export const elements = {
   projectsList: document.getElementById('projects'),
-  projectModalBtn: document.querySelector('.btn-project-modal'),
   addProjectBtn: document.querySelector('.btn-add-project')
 };
