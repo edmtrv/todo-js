4f30290aba0124b7f62242038bfe60fed201e4f8
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-12 00:46:59 +0200

    Clean up test data

--- src/js/index.js
+++ src/js/index.js
@@ -9,18 +9,6 @@ import * as projectView from './views/projectView';
 
 const state = {};
 
-// const t1 = new Todo('My first todo', 'Doing something cool', '20191112');
-// const t2 = new Todo('My second todo', 'A lot of work', '20191209');
-// const t3 = new Todo('My third todo', 'Doing something even better', '20191125');
-
-// p1.addTodo(t1);
-// p1.addTodo(t2);
-// p1.addTodo(t3);
-
-// state.projects.push(p1);
-
-// console.dir(state);
-
 window.addEventListener('load', () => {
   state.projects = [];
   const defaultProject = new Project('Default Project', 'My project for testing purposes');
