64f99a040b4ced9a8472877d0ba3b55bd384b715
Emil Dimitrov <edimitrov1@gmail.com>
2019-12-12 12:26:27 +0200

    Add queries to the elements

--- src/js/tools.js
+++ src/js/tools.js
@@ -3,5 +3,8 @@ export const elements = {
   projectForm: document.querySelector('.project-form'),
   projectDetails: document.querySelector('.project-details'),
   todosList: document.querySelector('.todos'),
-  todoForm: document.querySelector('.todo-form')
+  todoForm: document.querySelector('.todo-form'),
+  todoComplete: document.querySelector('.btn-complete-todo'),
+  todoEdit: document.querySelector('.btn-edit-todo'),
+  todoDelete: document.querySelector('.btn-delete-todo')
 };
